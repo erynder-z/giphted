5cf5df817c3675d16f0833f614e5943e25c9fe9a
eryner-z <stfn.bgr@gmail.com>
2022-02-03 19:47:11 +0100

    refactor search

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const fetchImage = () => {
+const fetchImage = (query) => {
   const img = document.querySelector('img');
+  let searchString;
+
+  if (!query) {
+    searchString = 'dog';
+  } else {
+    searchString = query.toLowerCase();
+  }
 
   fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=hbjUgiRex7neX4tpv0Tf5xFT4HzBCmpm&s=dogs',
+    `https://api.giphy.com/v1/gifs/translate?api_key=hbjUgiRex7neX4tpv0Tf5xFT4HzBCmpm&s=${searchString}`,
     { mode: 'cors' }
   )
     .then((response) => response.json())
@@ -26,22 +33,8 @@ const search = (() => {
   searchButton.addEventListener('click', () => {
     const query = searchbar.value;
     searchbar.value = '';
-    searchImage(query);
+    fetchImage(query);
   });
 })();
-
-const searchImage = (query) => {
-  const img = document.querySelector('img');
-  const searchString = query.toLowerCase();
-
-  fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=hbjUgiRex7neX4tpv0Tf5xFT4HzBCmpm&s=${searchString}`,
-    { mode: 'cors' }
-  )
-    .then((response) => response.json())
-    .then((response) => {
-      img.src = response.data.images.original.url;
-    });
-};
 
 fetchImage();
