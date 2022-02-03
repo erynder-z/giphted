ddb6ec5fd05e7ecfefe86cdfe240a5ae32403b4e
eryner-z <stfn.bgr@gmail.com>
2022-02-03 19:40:19 +0100

    search function

--- script.js
+++ script.js
@@ -19,4 +19,29 @@ const refreshbutton = (() => {
   });
 })();
 
+const search = (() => {
+  const searchbar = document.querySelector('input');
+  const searchButton = document.getElementById('searchBtn');
+
+  searchButton.addEventListener('click', () => {
+    const query = searchbar.value;
+    searchbar.value = '';
+    searchImage(query);
+  });
+})();
+
+const searchImage = (query) => {
+  const img = document.querySelector('img');
+  const searchString = query.toLowerCase();
+
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=hbjUgiRex7neX4tpv0Tf5xFT4HzBCmpm&s=${searchString}`,
+    { mode: 'cors' }
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      img.src = response.data.images.original.url;
+    });
+};
+
 fetchImage();
